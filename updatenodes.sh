b35b7789f45b3b50b9f573ac1e9a2aa00defe207
Dmitrij Kaliberda <dmitry.k@webmeridian.net>
2019-12-24 19:01:16 +0200

    rewrite updatenodes in bash

---
+++ updatenodes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROBE_FILE="/fb_host_probe.txt"
+VARNISHADM="varnishadm -T varnish:6082 -S /varnish.secret"
+REDIS_CLI="redis-cli -h clusterdata"
+
+
+VARNISH_STATUS=$($VARNISHADM ping | awk '{print $1}')
+if [[ ! $VARNISH_STATUS == "PONG" ]]; then
+  echo "Unable to read VCLs from Varnish"
+  exit 1
+fi
+
+VCL_NAME=$($VARNISHADM vcl.list | awk '{print $4}')
+$VARNISHADM vcl.show $VCL_NAME > /root/vcl.vcl
